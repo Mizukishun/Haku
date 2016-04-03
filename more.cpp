8a6498f5c139b7869695fe8cef85bb2a00228a4e
StarGazer <wangdchun@qq.com>
2016-04-04 01:37:36 +0800

    在“更多”窗体中增添了打开Haku源代码的按钮

--- more.cpp
+++ more.cpp
@@ -7,9 +7,9 @@ More::More(QWidget *parent) : QWidget(parent)
     setContentsMargins(0, 0, 0, 0);
     //设置固定的宽度
     setFixedWidth(300);
-    //设置背景颜色
+    //设置背景颜色为半透明的
     QPalette Mop;
-    Mop.setColor(QPalette::Background, Qt::green);
+    Mop.setColor(QPalette::Background, QColor(218, 230, 241, 100));
     setAutoFillBackground(true);
     setPalette(Mop);
 
@@ -22,16 +22,40 @@ void More::createInterface()
 {
     //主布局
     mainLayout = new QVBoxLayout(this);
+    mainLayout->setSpacing(20);
+    mainLayout->setMargin(20);
 
     //启动酷狗音乐的按钮
     openKugouBtn = new QPushButton;
-    openKugouBtn->setText(tr("酷狗音乐"));
+    //openKugouBtn->setText(tr("酷狗音乐"));
+    openKugouBtn->setFlat(true);
+    openKugouBtn->setIcon(QPixmap(":/images/kugou2.png"));
+    openKugouBtn->setIconSize(QPixmap(":/images/kugou2.png").size());
+    openKugouBtn->setToolTip(tr("打开酷狗音乐"));
     //关联启动酷狗音乐的函数
     connect(openKugouBtn, SIGNAL(clicked()), this, SLOT(openKugou()));
 
+    //用默认浏览器打开这个项目的GitHub页面
+    openMyGitHubBtn = new QPushButton;
+    //openMyGitHubBtn->setText(tr("源代码"));
+    openMyGitHubBtn->setFlat(true);
+    openMyGitHubBtn->setIcon(QPixmap(":/images/github2.png"));
+    openMyGitHubBtn->setIconSize(QPixmap(":/images/github2.png").size());
+    openMyGitHubBtn->setToolTip(tr("打开GitHub上的Haku源代码"));
+    connect(openMyGitHubBtn, SIGNAL(clicked()), this, SLOT(openMyGitHub()));
 
-    //将按钮添加到布局中
-    mainLayout->addWidget(openKugouBtn);
+    //将上面两个按钮添加到第一行小布局中
+    firstLineLayout = new QHBoxLayout;
+    firstLineLayout->setMargin(0);
+    firstLineLayout->setSpacing(0);
+    firstLineLayout->addStretch();
+    firstLineLayout->addWidget(openKugouBtn);
+    firstLineLayout->addStretch();
+    firstLineLayout->addWidget(openMyGitHubBtn);
+    firstLineLayout->addStretch();
+
+    //将小布局添加到主布局中
+    mainLayout->addLayout(firstLineLayout);
     mainLayout->addStretch();
 }
 
@@ -49,4 +73,10 @@ void More::openKugou()
     //因为我的电脑是windows的，所以是\\
 
     //注意，这里还没有错误处理，所以之后得补充一个错误处理的东东
+}
+
+//启动默认浏览器打开Haku项目的GitHub页面
+void More::openMyGitHub()
+{
+    QDesktopServices::openUrl(QUrl("https://github.com/DchunWang/Haku"));
 }
--- more.h
+++ more.h
@@ -5,7 +5,12 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QGridLayout>
 #include <QProcess>
+#include <QUrl>
+#include <QDesktopServices>
+#include <QImage>
+#include <QPixmap>
 
 
 
@@ -26,13 +31,19 @@ signals:
 public slots:
     //启动一个外部程序，这个函数是启动酷狗音乐
     void openKugou();
+    //启动默认浏览器打开我这个Haku项目的GitHub页面
+    void openMyGitHub();
 
 private:
     //点击这个按钮就启动酷狗音乐
     QPushButton *openKugouBtn;
+    //点击这个按钮启动默认浏览器，打开我的GitHub主页
+    QPushButton *openMyGitHubBtn;
 
     //主布局
     QVBoxLayout *mainLayout;
+    //第一行的小布局
+    QHBoxLayout *firstLineLayout;
 
     //要启动的酷狗进程
     QProcess *kugouProcess;
